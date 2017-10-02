4d6d34c076d7731c04c5c2a35b87c960e0b01749
hareeshSimma <hareesh.simma@gmail.com>
2017-10-02 17:41:05 +0530

    last commit

--- src/app/affiliatedunits/affiliatedunits.component.ts
+++ src/app/affiliatedunits/affiliatedunits.component.ts
@@ -8,30 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class AffiliatedunitsComponent implements OnInit {
    value:string;
    public temp:string="";
-   //ant:boolean=true;
-  // public status:any={
-  //    ananth :false,
-  //    chit:false,
-  //    ysr:false,
-  //    kurn:false,
-  //    east:false,
-  //    gunt:false,
-  //   kris:false,
-  //   nel:false,
-  //   prak:false,
-  //   srik:false,
-  //   vsk:false,
-  //   vzg:false,
-  //   west:false
-  // }
-  constructor() {this.value=""; }
+   
+   
+  
+  constructor() {
+
+  this.value="";
+
+   }
 
   ngOnInit() {
   }
 
   public onClick(i){
-    console.log(i);
-    // this.ant=false;
+    
   
     var idAttr = i.target.attributes.id;
     this.value = idAttr.nodeValue;
